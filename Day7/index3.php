686203c67f4bd740f971265ef8ad2612904279f5
TanishaSharma09 <tanisha4623230@gmail.com>
2026-07-09 13:24:26 +0530

    upload

---
+++ Day7/index3.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello world";
+$student = ["Rahul", "Jitesh", "Ram"];
+print_r($student);
+$student_marks = [12, 34, 45];
+print_r($student_marks);
+$students = [
+    "Rohan" => 12,
+    "Jitesh" => 34
+    
+];
+print_r($students["Rohan"]);
+echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";
+
+
+?>

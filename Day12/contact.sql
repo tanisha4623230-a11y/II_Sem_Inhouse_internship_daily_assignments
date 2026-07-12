0e1a4f6266a727e3e6e552ab5aba65c92412f24d
TanishaSharma09 <tanisha4623230@gmail.com>
2026-07-12 21:27:48 +0530

    upload

---
+++ Day12/contact.sql
@@ -0,0 +1,26 @@
+contacts
+----------------
+name
+class_semester 
+email_address
+city
+mobile
+message
+created_at
+updated_at
+inserted into contacts(class_semester, name, email_address, city, mobile, message) values ('3rd sem ','test@example.com','jaipur','9866746776','skit','i need to know more about the bootcamp');
+insert into contacts
+set
+name='mahesh',
+class_semester='3rd sem',
+email_address='test@example.com',
+city='jaipur',
+mobile='9866746776',
+message='hello',
+collage='skit',
+created_at=NOW(),
+updated_at=NOW()
+;
+
+
+
